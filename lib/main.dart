cc848551345cd11d52c34229ee188bd903c12fdc
Amitkumarkoli <akdev523@gmail.com>
2024-07-26 01:09:25 +0530

    add some minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:ngo_scout/core/providers/welcome_provider.dart';
 import 'package:ngo_scout/domain/providers/auth_provider.dart';
-import 'package:ngo_scout/features/presentation/screens/scrollable_content_screen.dart';
-import 'package:ngo_scout/features/presentation/screens/search_screen.dart';
-import 'package:ngo_scout/features/presentation/screens/people_profile.dart';
-import 'package:ngo_scout/features/presentation/screens/welcome_screen.dart';
-import 'package:ngo_scout/features/presentation/screens/login_screen.dart';
-import 'package:ngo_scout/features/presentation/screens/signup_screen.dart';
-import 'package:ngo_scout/features/presentation/screens/people_home_screen.dart';
 import 'package:ngo_scout/routes/routes.dart';
 import 'package:provider/provider.dart';
 
